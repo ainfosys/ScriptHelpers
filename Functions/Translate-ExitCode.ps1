b97c8a2955422e9c108cc01922e080957d5d5cbe
Ryan McAvoy <rmcavoy@ainfosys.com>
2022-01-25 08:48:59 -0500

    added Translate-ExitCode function script

---
+++ Functions/Translate-ExitCode.ps1
@@ -0,0 +1,45 @@
+﻿function Translate-ExitCode
+{
+	param
+	(
+		[parameter(Mandatory = $true)]
+		$Process
+	)
+    
+    $Errorxmlpath = "C:\Windows\temp\test.xml"
+    if(!(test-path $Errorxmlpath)){
+	    Invoke-WebRequest -uri "https://raw.githubusercontent.com/ainfosys/ScriptHelpers/main/Files/ms-error-codes.xml" -OutFile "C:\Windows\temp\test.xml"
+    }
+
+	[xml]$ErrorXml = Get-Content "C:\Windows\temp\test.xml"
+	$SpecifiedError = $ErrorXml.Root.row | where { $_.code -eq $($Process) }
+	
+	# append to psobject, add property to indicate if the exit code means failure or not
+	switch ($($SpecifiedError.Code)) {
+		"0" {
+			$Success = $true
+		}
+		"1641" {
+			$Success = $true
+		}
+		"3010" {
+			$Success = $true
+		}
+		"3011" {
+			$Success = $true
+		}
+		default {
+			$Success = $false
+		}
+	}
+	
+	$ErrorInfo = @{
+		Code = $($SpecifiedError.Code)
+		Description = $($SpecifiedError.Description)
+		Success = $Success
+	}
+    
+
+    # Return the information on the exit code
+    Return $($ErrorInfo | ConvertTo-Json | ConvertFrom-Json)
+}
