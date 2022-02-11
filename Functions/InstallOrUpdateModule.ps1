5c22964413d8d9b0048af2b5ad7adf9eba808541
Ryan McAvoy <rmcavoy@ainfosys.com>
2022-02-11 09:35:38 -0500

    Added InstallOrUpdateModule function

---
+++ Functions/InstallOrUpdateModule.ps1
@@ -0,0 +1,40 @@
+﻿Function InstallOrUpdateModule {
+    param
+    (
+	    [parameter(Mandatory = $true)]
+        [String]
+	    $ModuleName
+    )
+    # this function is a simple way of making sure the desired module is installed and up to date
+    # Enter the module name as found in the ps gallery as the parameter. Ex: RunAsUser (not case sensitive)
+    # TODO: Provide the option to allow verbose output via parameter
+
+    # Set TLS to 1.2 for this Powershell session. This will not work on Windows 7 or lower.
+    [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+
+    # Make sure package providers are up to date
+    $LocalNugetVersion = Get-PackageProvider -Name nuget | select -expand Version
+    $LocalPowershellGetVersion = Get-PackageProvider -name PowershellGet | select -expand Version
+    $LatestNugetVersion = Find-PackageProvider -Name nuget | select -expand Version
+    $LatestPowershellGetVersion = Find-PackageProvider -name PowershellGet | select -expand Version
+
+    if($LocalNugetVersion -lt $LatestNugetVersion){
+        Install-PackageProvider -Name NuGet -Force -Confirm:$False | Out-Null
+    }
+
+    if($LocalPowershellGetVersion -lt $LatestPowershellGetVersion){
+        Install-Module -Name PowerShellGet -Force -Confirm:$False | Out-Null
+    }
+
+    if ($(Get-Module -ListAvailable | select -expand Name) -inotcontains $ModuleName)
+    {
+        # if the module isn't found to be installed it will install it here
+	    Install-Module -Name $ModuleName -Force -Confirm:$False | Out-Null
+    }
+    elseif($(Get-Module -ListAvailable | Where {$_.name -eq $ModuleName} | select -expand Version) -lt $(Find-Module -name $ModuleName | select -expand Version)){
+    
+        # The local version is out of date, update it here
+        Update-module -Name $ModuleName -Force -Confirm:$False | Out-Null
+    }
+    Import-Module -name $ModuleName
+}
---
+++ Functions/Translate-WindowsBuild.TempPoint.ps1
@@ -0,0 +1,58 @@
+﻿Function Translate-WindowsBuild
+{
+	<#
+	.DESCRIPTION: Provides information on the Windows build based on the given parameter
+	.AUTHOR: Ryan
+	#>
+	
+	param
+	(
+		[parameter(Mandatory = $false)]
+		$Build,
+		[parameter(Mandatory = $false)]
+		$Version
+	)
+	
+	if ([string]::IsNullOrWhiteSpace($Build) -and [string]::IsNullOrWhiteSpace($Version))
+	{
+		Write-Host "At least one parameter is required (build or version)"; Break
+	}
+	
+	$WininfoXMLpath = "C:\Windows\Temp\WinInfo.xml"
+	Invoke-WebRequest -Uri "https://raw.githubusercontent.com/ainfosys/ScriptHelpers/main/Files/Windows10-11-VersionHistory.xml" -OutFile $WininfoXMLpath
+	
+	[xml]$WinInfoXML = Get-Content $WininfoXMLpath
+	
+	if (![string]::IsNullOrWhiteSpace($Build) -and ![string]::IsNullOrWhiteSpace($Version))
+	{
+		$Info = $WinInfoXML | where { $_.Windows.info.build -eq $Build }
+		
+		if ($Info.windows.info.version -ne $Version)
+		{
+			Write-Host "Both parameters are provided but the values do not match expected values."
+			Write-Host "Provided build: $Build"
+			Write-Host "Provided version: $Version"
+			Write-Host "Expected version: $($Info.windows.info.version)"; Break
+		}
+	}
+	elseif (![string]::IsNullOrWhiteSpace($Build) -and [string]::IsNullOrWhiteSpace($Version))
+	{
+		$Info = $WinInfoXML | where { $_.Windows.info.build -eq $Build }
+	}
+	elseif ([string]::IsNullOrWhiteSpace($Build) -and ![string]::IsNullOrWhiteSpace($Version))
+	{
+		$Info = $WinInfoXML | where { $_.Windows.info.version -eq $Version }
+	}
+	
+	$WindowsInfo = @{
+		Build = $Info.windows.info.Build
+		Version = $Info.windows.info.version
+		ReleaseDate = $Info.windows.info.release_Date
+		Standard_EOL = $Info.windows.info.GAC_EOL
+		Enterprise_EOL = $Info.windows.info.GAC_Enterprise_EOL
+		LTSC_EOL = $Info.windows.info.LTSC_EOL
+		MajorVersion = $Info.windows.info.Major_version
+	}
+	
+	Return $($WindowsInfo| ConvertTo-Json | ConvertFrom-Json)
+}
