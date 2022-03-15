6dbff182975d5900d1a20f809b985d6423d9c182
Ryan McAvoy <rmcavoy@ainfosys.com>
2022-03-15 12:58:38 -0400

    fixed issues with Translate-WindowsBuild function

--- Functions/Translate-WindowsBuild.ps1
+++ Functions/Translate-WindowsBuild.ps1
@@ -7,51 +7,76 @@
 	
 	param
 	(
+		[parameter(Mandatory = $false, HelpMessage = 'Use the following to get the build number: $([System.Environment]::OSVersion.Version.Build)')]
+		[int]$Build,
+		[parameter(Mandatory = $false, HelpMessage = "The 'Friendly Name' for build numbers. EX: 20H2")]
+		[string]$Version,
 		[parameter(Mandatory = $false)]
-		$Build,
-		[parameter(Mandatory = $false)]
-		$Version
+		[Switch]$Win10Only = $false
 	)
-	
-	if ([string]::IsNullOrWhiteSpace($Build) -and [string]::IsNullOrWhiteSpace($Version))
-	{
-		Write-Host "At least one parameter is required (build or version)"; Break
-	}
 	
 	$WininfoXMLpath = "C:\Windows\Temp\WinInfo.xml"
 	Invoke-WebRequest -Uri "https://raw.githubusercontent.com/ainfosys/ScriptHelpers/main/Files/Windows10-11-VersionHistory.xml" -OutFile $WininfoXMLpath
 	
 	[xml]$WinInfoXML = Get-Content $WininfoXMLpath
 	
-	if (![string]::IsNullOrWhiteSpace($Build) -and ![string]::IsNullOrWhiteSpace($Version))
+	$boolCheck = [bool]$Build
+	if (!($boolCheck))
 	{
-		$Info = $WinInfoXML | where { $_.Windows.info.build -eq $Build }
-		
-		if ($Info.windows.info.version -ne $Version)
+		$boolCheck = [bool]$Version
+		if (!($boolCheck))
 		{
-			Write-Host "Both parameters are provided but the values do not match expected values."
-			Write-Host "Provided build: $Build"
-			Write-Host "Provided version: $Version"
-			Write-Host "Expected version: $($Info.windows.info.version)"; Break
+			Write-Host "At least one parameter is required (build or version)"; Break
+		}
+		else
+		{
+			# just version provided
+			$Info = $WinInfoXML.root.buildinfo | where {$_.version -eq $Version}	
 		}
 	}
-	elseif (![string]::IsNullOrWhiteSpace($Build) -and [string]::IsNullOrWhiteSpace($Version))
+	else
 	{
-		$Info = $WinInfoXML | where { $_.Windows.info.build -eq $Build }
+		$boolCheck = [bool]$Version
+		if ($boolCheck)
+		{
+			# Build and version provided
+			$Info = $WinInfoXML.root.buildinfo | where { $_.build -eq $Build }
+			
+			if ($Info.windows.info.version -ne $Version)
+			{
+				Write-Host "Both parameters are provided but the values do not match expected values."
+				Write-Host "Provided build: $Build"
+				Write-Host "Provided version: $Version"
+				Write-Host "Expected version: $($Info.version)"; Break
+			}
+		}
+		else
+		{
+			# just build provided
+			$Info = $WinInfoXML.root.buildinfo | where { $_.build -eq $Build }
+		}
 	}
-	elseif ([string]::IsNullOrWhiteSpace($Build) -and ![string]::IsNullOrWhiteSpace($Version))
+	
+	if ($Win10Only)
 	{
-		$Info = $WinInfoXML | where { $_.Windows.info.version -eq $Version }
+		# Exclude windows 11 info from the return info
+		$Info = $Info | where { $_.Majorversion -ne "11" }
+		
+		# Note the following may fail on devices without the proper version of .net installed
+		if ([string]::IsNullOrWhiteSpace($($Info.build)))
+		{
+			Write-Output -InputObject "Only Windows 11 infomation found. Windows 11 information selected to be excluded so no information can be returned."; Break
+		}
 	}
 	
 	$WindowsInfo = @{
-		Build = $Info.windows.info.Build
-		Version = $Info.windows.info.version
-		ReleaseDate = $Info.windows.info.release_Date
-		Standard_EOL = $Info.windows.info.GAC_EOL
-		Enterprise_EOL = $Info.windows.info.GAC_Enterprise_EOL
-		LTSC_EOL = $Info.windows.info.LTSC_EOL
-		MajorVersion = $Info.windows.info.Major_version
+		Build = $Info.Build
+		Version = $Info.version
+		ReleaseDate = $Info.releaseDate
+		Standard_EOL = $Info.GACEOL
+		Enterprise_EOL = $Info.GACEnterpriseEOL
+		LTSC_EOL = $Info.LTSCEOL
+		MajorVersion = $Info.Majorversion
 	}
 	
 	Return $($WindowsInfo| ConvertTo-Json | ConvertFrom-Json)
