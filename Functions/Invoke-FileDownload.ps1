6d95b726e7eb90ac64078b766f5a9d5360576a8e
Dark_Web_Dennis <61989372+3pichaxz0r@users.noreply.github.com>
2024-01-19 15:31:11 -0500

    Update Invoke-FileDownload.ps1
    added check to make sure parent path to destination exists

--- Functions/Invoke-FileDownload.ps1
+++ Functions/Invoke-FileDownload.ps1
@@ -9,30 +9,35 @@ function Invoke-FileDownload {
     # set tls policy to tls12 and tls13
     [Net.ServicePointManager]::SecurityProtocol = 15360
 
+    # validate the parent directory of the destination
+    $parentpath = Split-path $destination -parent
+    if(!(test-path $parentpath)){
+        throw "Invoke-FileDownload: Parent path of provided destination does not exist"
+    }
+
     # validate the provided destination file name
     $IndexOfInvalidChar = $destination.IndexOfAny([System.IO.Path]::GetInvalidFileNameChars())
     if($IndexOfInvalidChar -eq -1){
-        Write-Warning "Invalid file name provided with destination path"
-        throw
+        throw "Invoke-FileDownload: Invalid file name provided with destination path"
     }
     try{
-        Write-Verbose "Attempting file transfer using BITS"
+        Write-Verbose "Invoke-FileDownload: Attempting file transfer using BITS"
         $bitsResult = Start-BitsTransfer -TransferType Download -Destination $destination -Source $source -ErrorAction stop
     }
     catch{
         try{
-            Write-Verbose "BITS transfer failed, attempting file transfer using IWR"
+            Write-Verbose "Invoke-FileDownload: BITS transfer failed, attempting file transfer using IWR"
             $IWRResult = Invoke-WebRequest -UseBasicParsing -Uri $source -OutFile $destination -ErrorAction Stop
         }
         catch{
             try{
-                Write-Verbose "IWR transfer failed, attempting file transfer using cURL"
+                Write-Verbose "Invoke-FileDownload: IWR transfer failed, attempting file transfer using cURL"
                 Start-Process curl -ArgumentList "-sLo $destination $source" -wait -ErrorAction stop
                 # double check if the file was successfully downloaded
                 if(test-path $destination){
-                    Write-Verbose "cURL file transfer appears successful"
+                    Write-Verbose "Invoke-FileDownload: cURL file transfer appears successful"
                 }else{
-                    Write-Verbose "cURL file transfer appears to have failed"
+                    Write-Verbose "Invoke-FileDownload: cURL file transfer appears to have failed"
                     throw
                 }
             }
@@ -41,10 +46,7 @@ function Invoke-FileDownload {
                 (New-Object System.Net.WebClient).DownloadFile($source, $destination)
                 # double check if the file was successfully downloaded
                 if(test-path $destination){
-                    Write-Verbose "WebClient object file transfer appears successful"
-                }else{
-                    Write-Verbose "WebClient object file transfer appears to have failed"
-                    throw
+                    throw "Invoke-FileDownload: All download methods have failed"
                 }
             }
         }
