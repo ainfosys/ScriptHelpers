41fe302240e6ee05a7152c4d5abbfab2386fea5d
Dark_Web_Dennis <61989372+3pichaxz0r@users.noreply.github.com>
2024-05-09 10:07:46 -0400

    Update Invoke-FileDownload.ps1
    Added option to disable progress output, added output of elapsed time and download method

--- Functions/Invoke-FileDownload.ps1
+++ Functions/Invoke-FileDownload.ps1
@@ -1,9 +1,14 @@
 function Invoke-FileDownload {
+    [cmdletbinding()]
     param(
-        [parameter(Mandatory)]
+        [parameter(Mandatory, HelpMessage = "URL or File share path")]
         $source,
-        [parameter(Mandatory)]
-        $destination
+        [parameter(Mandatory, HelpMessage = "Full path to file destination including file name")]
+        $destination,
+        [parameter(HelpMessage = "Determines if progress bars are shown for Invoke-Webrequest and Start-BitsTransfer")]
+        #https://learn.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_preference_variables?view=powershell-5.1#progresspreference
+        [validateset("SilentlyContinue","Continue","Stop","Ignore")]
+        $ProgressPreference = 'SilentlyContinue' # SilentlyContinue will speed up download speeds
     )
 
     try{
@@ -25,34 +30,73 @@ function Invoke-FileDownload {
     if($IndexOfInvalidChar -eq -1){
         throw "Invoke-FileDownload: Invalid file name provided with destination path"
     }
+
+    # attempt download
     try{
+        # BITS
         Write-Verbose "Invoke-FileDownload: Attempting file transfer using BITS"
-        $bitsResult = Start-BitsTransfer -TransferType Download -Destination $destination -Source $source -ErrorAction stop
+        $stopwatch = New-Object -TypeName 'System.Diagnostics.Stopwatch'
+        $stopwatch.Start()
+        Start-BitsTransfer -TransferType Download -Destination $destination -Source $source -ErrorAction stop
+        $stopwatch.Stop()
+        $return = @{
+            "Download_Function" = "BITS"
+            "Total_Time" = $stopwatch.Elapsed
+            "Output" = $destination
+        }
+        return $return
     }
     catch{
         try{
-            Write-Verbose "Invoke-FileDownload: BITS transfer failed, attempting file transfer using IWR"
+            # IWR
+            Write-Verbose "Invoke-FileDownload: BITS transfer failed, attempting file transfer using Invoke-WebRequest"
+            $stopwatch = New-Object -TypeName 'System.Diagnostics.Stopwatch'
+            $stopwatch.Start()
             $IWRResult = Invoke-WebRequest -UseBasicParsing -Uri $source -OutFile $destination -ErrorAction Stop
+            $stopwatch.Stop()
+            $return = @{
+                "Download_Function" = "Invoke-WebRequest"
+                "Total_Time" = $stopwatch.Elapsed
+                "Output" = $destination
+            }
+            return $return
         }
         catch{
             try{
-                Write-Verbose "Invoke-FileDownload: IWR transfer failed, attempting file transfer using cURL"
-                Start-Process curl -ArgumentList "-sLo $destination $source" -wait -ErrorAction stop
+                # cURL
+                Write-Verbose "Invoke-FileDownload: Invoke-WebRequest transfer failed, attempting file transfer using cURL"
+                $stopwatch = New-Object -TypeName 'System.Diagnostics.Stopwatch'
+                $stopwatch.Start()
+                $process = Start-Process curl -ArgumentList "-sLo $destination $source" -wait -ErrorAction stop
+                $stopwatch.Stop()
                 # double check if the file was successfully downloaded
-                if(test-path $destination){
-                    Write-Verbose "Invoke-FileDownload: cURL file transfer appears successful"
-                }else{
-                    Write-Verbose "Invoke-FileDownload: cURL file transfer appears to have failed"
-                    throw
+                if(!(test-path $destination)){
+                    Throw "Invoke-FileDownload: cURL file transfer appears to have failed"
                 }
+                $return = @{
+                    "Download_Function" = "cURL"
+                    "Total_Time" = $stopwatch.Elapsed
+                    "Output" = $destination
+                }
+                return $return
             }
             catch{
-                Write-Verbose "cURL transfer failed, attempting webclient object file transfer"
+                # Webclient object
+                Write-Verbose "Invoke-FileDownload: cURL transfer failed, attempting webclient object file transfer"
+                $stopwatch = New-Object -TypeName 'System.Diagnostics.Stopwatch'
+                $stopwatch.Start()
                 (New-Object System.Net.WebClient).DownloadFile($source, $destination)
+                $stopwatch.Stop()
                 # double check if the file was successfully downloaded
-                if(test-path $destination){
+                if(!(test-path $destination)){
                     throw "Invoke-FileDownload: All download methods have failed"
                 }
+                $return = @{
+                    "Download_Function" = "WebClient Object"
+                    "Total_Time" = $stopwatch.Elapsed
+                    "Output" = $destination
+                }
+                return $return
             }
         }
     }
