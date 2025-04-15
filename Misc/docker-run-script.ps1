075354643bf54c895b1e437bffd1ab8c47621330
Dark_Web_Dennis <61989372+3pichaxz0r@users.noreply.github.com>
2025-04-15 18:03:51 -0400

    Update docker-run-script.ps1

--- Misc/docker-run-script.ps1
+++ Misc/docker-run-script.ps1
@@ -7,14 +7,18 @@ catch{
 }
 
 # get latest vscodium
-new-item -path 'c:\vscodium' -itemtype directory -force;
-$latest = $(irm "https://api.github.com/repos/VSCodium/vscodium/releases")[0].assets | where { $_.name -ilike "vscodium-reh-web-win32*.tar.gz" } | select -expand browser_download_url;
-curl -L $latest --output C:\vscodium\vscodium.tar.gz;
+new-item -path 'c:\vscodium' -itemtype directory -force | out-null
+$latest = $(irm "https://api.github.com/repos/VSCodium/vscodium/releases")[0].assets | where { $_.name -ilike "vscodium-reh-web-win32*.tar.gz" } | select -expand browser_download_url
+write-host "~Downloading latest version of VSCodium web server"
+curl -L $latest --output C:\vscodium\vscodium.tar.gz
 
 # get latest powershell core
-$latestPwsh = $($(IRM "https://api.github.com/repos/PowerShell/PowerShell/releases/latest")[0].assets | where { $_.name -ilike "*x64.msi" }).browser_download_url;
-curl -L $latestPwsh -outfile "$ENV:Temp\pwsh.msi";
-start-process msiexec -argumentlist "/i `"$ENV:Temp\pwsh.msi`" /qn /norestart" -wait;
+$latestPwsh = $($(IRM "https://api.github.com/repos/PowerShell/PowerShell/releases/latest")[0].assets | where { $_.name -ilike "*x64.msi" }).browser_download_url
+Write-host "~Downloading latest Powershell core installer"
+curl -L $latestPwsh -outfile "$ENV:Temp\pwsh.msi"
+Write-host "~Installing Powershell Core"
+start-process msiexec -argumentlist "/i `"$ENV:Temp\pwsh.msi`" /qn /norestart" -wait
 
 # extract vscodium
+Write-host "~Extracting VSCodium"
 tar -xvf "C:\vscodium\vscodium.tar.gz" -C C:\vscodium
