3991f101c774088cb2d13a04119c64048d80a3f0
Dark_Web_Dennis <61989372+3pichaxz0r@users.noreply.github.com>
2025-04-15 17:55:37 -0400

    Update docker-run-script.ps1

--- Misc/docker-run-script.ps1
+++ Misc/docker-run-script.ps1
@@ -6,17 +6,14 @@ catch{
     [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Tls12
 }
 
-# if the progress preference isnt set downloads will display a lot of crap output
-$ProgressPreference = SilentlyContinue
-
 # get latest vscodium
 new-item -path 'c:\vscodium' -itemtype directory -force;
 $latest = $(irm "https://api.github.com/repos/VSCodium/vscodium/releases")[0].assets | where { $_.name -ilike "vscodium-reh-web-win32*.tar.gz" } | select -expand browser_download_url;
-iwr -UseBasicParsing $latest -outfile "C:\vscodium\vscodium.tar.gz";
+curl -L $latest --output C:\vscodium\vscodium.tar.gz;
 
 # get latest powershell core
 $latestPwsh = $($(IRM "https://api.github.com/repos/PowerShell/PowerShell/releases/latest")[0].assets | where { $_.name -ilike "*x64.msi" }).browser_download_url;
-iwr -usebasicparsing $latestPwsh -outfile "$ENV:Temp\pwsh.msi";
+curl -L $latestPwsh -outfile "$ENV:Temp\pwsh.msi";
 start-process msiexec -argumentlist "/i `"$ENV:Temp\pwsh.msi`" /qn /norestart" -wait;
 
 # extract vscodium
