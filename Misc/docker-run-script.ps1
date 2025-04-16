fec5b26128ba06578f8dee464a01a178d192c457
Dark_Web_Dennis <61989372+3pichaxz0r@users.noreply.github.com>
2025-04-15 21:33:36 -0400

    Update docker-run-script.ps1

--- Misc/docker-run-script.ps1
+++ Misc/docker-run-script.ps1
@@ -9,17 +9,20 @@ catch{
 # get latest vscodium
 new-item -path 'c:\vscodium' -itemtype directory -force | out-null
 $latest = $(irm "https://api.github.com/repos/VSCodium/vscodium/releases")[0].assets | where { $_.name -ilike "vscodium-reh-web-win32*.tar.gz" } | select -expand browser_download_url
-write-host "~Downloading latest version of VSCodium web server"
+write-host "~ Downloading latest version of VSCodium web server"
 . "C:\Windows\System32\curl.exe" -L $latest --output C:\vscodium\vscodium.tar.gz
 
 # get latest powershell core
 $latestPwsh = $($(IRM "https://api.github.com/repos/PowerShell/PowerShell/releases/latest")[0].assets | where { $_.name -ilike "*x64.msi" }).browser_download_url
-Write-host "~Downloading latest Powershell core installer"
+Write-host "~ Downloading latest Powershell core installer"
 . "C:\Windows\System32\curl.exe" -L $latestPwsh -outfile "$ENV:Temp\pwsh.msi"
-Write-host "~Installing Powershell Core"
+Write-host "~ Installing Powershell Core"
 $process = start-process msiexec -argumentlist "/i `"$ENV:Temp\pwsh.msi`" /qn /norestart" -wait -passthru
-Write-host "~Powershell install exit code: $($Process.exitcode)"
+Write-host "~ Powershell install exit code: $($Process.exitcode)"
+
+# add firewall exceptions for vscodium
+New-NetFirewallRule -Name 'vscodium' -DisplayName 'vscodium' -Enabled True -Direction Outbound -Protocol TCP -Action Allow -LocalPort 8000
 
 # extract vscodium
-Write-host "~Extracting VSCodium"
+Write-host "~ Extracting VSCodium"
 tar -xvf "C:\vscodium\vscodium.tar.gz" -C C:\vscodium
