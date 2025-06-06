839f6c2500c0f46fabe985fdb859ec4e694d6a46
Dark_Web_Dennis <61989372+3pichaxz0r@users.noreply.github.com>
2025-06-06 13:10:54 -0400

    Update Get-LockedFileProcess.ps1

--- Functions/Get-LockedFileProcess.ps1
+++ Functions/Get-LockedFileProcess.ps1
@@ -1,42 +1,232 @@
-function Get-LockedFileProcess {
-    param(
-        $FileName
+function Get-FileLockProcess {
+    [CmdletBinding()]
+    Param(
+        [Parameter(Mandatory=$True)]
+        [string]$FilePath
     )
-    $URL = "https://www.nirsoft.net/utils/ofview-x64.zip"
-    $Zip = "$($ENV:SystemDrive)\Windows\Temp\ofview-x64.zip"
-    $Exe = "$($ENV:SystemDrive)\Windows\Temp\OpenedFilesView.exe"
-    $ResultCsv = "$($ENV:SystemDrive)\Windows\Temp\results.csv"
 
-    if(test-path $ResultCsv){
-        Remove-item $ResultCsv -Force
+    ##### BEGIN Variable/Parameter Transforms and PreRun Prep #####
+
+    if (! $(Test-Path $FilePath)) {
+        Write-Error "The path $FilePath was not found! Halting!"
+        $global:FunctionResult = "1"
+        return
     }
 
-    # Force TLS 1.2 for this powershell session
-    [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+    ##### END Variable/Parameter Transforms and PreRun Prep #####
 
-    # download the required application
-    Try{
-        Invoke-WebRequest -UseBasicParsing -Uri $URL -OutFile $Zip -ErrorAction stop
-    }
-    catch{
-        Write-Warning "Failed to download required files. Error message follows:"
-        $Error[0]; Throw
-    }
 
-    $expand = Expand-Archive -Path $Zip -DestinationPath "$($ENV:SystemDrive)\Windows\Temp\" -Force
+    ##### BEGIN Main Body #####
 
-    Start-process $Exe -ArgumentList "/scomma $ResultCsv" -wait
-    #Write-output "Information may get cut off, view full results here: $ResultCSV"
-    $CSV = Import-Csv $ResultCsv -Header File,FilePath,ProcessHandle,CreatedTime,ModifiedTime,Attributes,FileSize,ReadAccess,WriteAccess,DeleteAccess,SharedRead,SharedWrite,SharedDelete,GrantedAccess,FilePosition,ProcessID,ProcessName,ProcessPath
-
-    $result = $CSV | where {$_.file -ieq $FileName}
-    if(![bool]$result){
-        try{
-            $result = $CSV | where {$_.file -ieq $($filename -replace "\..*")}
+    if ($PSVersionTable.PSEdition -eq "Desktop" -or $PSVersionTable.Platform -eq "Win32NT" -or 
+    $($PSVersionTable.PSVersion.Major -le 5 -and $PSVersionTable.PSVersion.Major -ge 3)) {
+        $CurrentlyLoadedAssemblies = [System.AppDomain]::CurrentDomain.GetAssemblies()
+    
+        $AssembliesFullInfo = $CurrentlyLoadedAssemblies | Where-Object {
+            $_.GetName().Name -eq "Microsoft.CSharp" -or
+            $_.GetName().Name -eq "mscorlib" -or
+            $_.GetName().Name -eq "System" -or
+            $_.GetName().Name -eq "System.Collections" -or
+            $_.GetName().Name -eq "System.Core" -or
+            $_.GetName().Name -eq "System.IO" -or
+            $_.GetName().Name -eq "System.Linq" -or
+            $_.GetName().Name -eq "System.Runtime" -or
+            $_.GetName().Name -eq "System.Runtime.Extensions" -or
+            $_.GetName().Name -eq "System.Runtime.InteropServices"
         }
-        catch{
+        $AssembliesFullInfo = $AssembliesFullInfo | Where-Object {$_.IsDynamic -eq $False}
+  
+        $ReferencedAssemblies = $AssembliesFullInfo.FullName | Sort-Object | Get-Unique
 
+        $usingStatementsAsString = @"
+        using Microsoft.CSharp;
+        using System.Collections.Generic;
+        using System.Collections;
+        using System.IO;
+        using System.Linq;
+        using System.Runtime.InteropServices;
+        using System.Runtime;
+        using System;
+        using System.Diagnostics;
+"@
+        
+        $TypeDefinition = @"
+        $usingStatementsAsString
+        
+        namespace MyCore.Utils
+        {
+            static public class FileLockUtil
+            {
+                [StructLayout(LayoutKind.Sequential)]
+                struct RM_UNIQUE_PROCESS
+                {
+                    public int dwProcessId;
+                    public System.Runtime.InteropServices.ComTypes.FILETIME ProcessStartTime;
+                }
+        
+                const int RmRebootReasonNone = 0;
+                const int CCH_RM_MAX_APP_NAME = 255;
+                const int CCH_RM_MAX_SVC_NAME = 63;
+        
+                enum RM_APP_TYPE
+                {
+                    RmUnknownApp = 0,
+                    RmMainWindow = 1,
+                    RmOtherWindow = 2,
+                    RmService = 3,
+                    RmExplorer = 4,
+                    RmConsole = 5,
+                    RmCritical = 1000
+                }
+        
+                [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Unicode)]
+                struct RM_PROCESS_INFO
+                {
+                    public RM_UNIQUE_PROCESS Process;
+        
+                    [MarshalAs(UnmanagedType.ByValTStr, SizeConst = CCH_RM_MAX_APP_NAME + 1)]
+                    public string strAppName;
+        
+                    [MarshalAs(UnmanagedType.ByValTStr, SizeConst = CCH_RM_MAX_SVC_NAME + 1)]
+                    public string strServiceShortName;
+        
+                    public RM_APP_TYPE ApplicationType;
+                    public uint AppStatus;
+                    public uint TSSessionId;
+                    [MarshalAs(UnmanagedType.Bool)]
+                    public bool bRestartable;
+                }
+        
+                [DllImport("rstrtmgr.dll", CharSet = CharSet.Unicode)]
+                static extern int RmRegisterResources(uint pSessionHandle,
+                                                    UInt32 nFiles,
+                                                    string[] rgsFilenames,
+                                                    UInt32 nApplications,
+                                                    [In] RM_UNIQUE_PROCESS[] rgApplications,
+                                                    UInt32 nServices,
+                                                    string[] rgsServiceNames);
+        
+                [DllImport("rstrtmgr.dll", CharSet = CharSet.Auto)]
+                static extern int RmStartSession(out uint pSessionHandle, int dwSessionFlags, string strSessionKey);
+        
+                [DllImport("rstrtmgr.dll")]
+                static extern int RmEndSession(uint pSessionHandle);
+        
+                [DllImport("rstrtmgr.dll")]
+                static extern int RmGetList(uint dwSessionHandle,
+                                            out uint pnProcInfoNeeded,
+                                            ref uint pnProcInfo,
+                                            [In, Out] RM_PROCESS_INFO[] rgAffectedApps,
+                                            ref uint lpdwRebootReasons);
+        
+                /// <summary>
+                /// Find out what process(es) have a lock on the specified file.
+                /// </summary>
+                /// <param name="path">Path of the file.</param>
+                /// <returns>Processes locking the file</returns>
+                /// <remarks>See also:
+                /// http://msdn.microsoft.com/en-us/library/windows/desktop/aa373661(v=vs.85).aspx
+                /// http://wyupdate.googlecode.com/svn-history/r401/trunk/frmFilesInUse.cs (no copyright in code at time of viewing)
+                /// 
+                /// </remarks>
+                static public List<Process> WhoIsLocking(string path)
+                {
+                    uint handle;
+                    string key = Guid.NewGuid().ToString();
+                    List<Process> processes = new List<Process>();
+        
+                    int res = RmStartSession(out handle, 0, key);
+                    if (res != 0) throw new Exception("Could not begin restart session.  Unable to determine file locker.");
+        
+                    try
+                    {
+                        const int ERROR_MORE_DATA = 234;
+                        uint pnProcInfoNeeded = 0,
+                            pnProcInfo = 0,
+                            lpdwRebootReasons = RmRebootReasonNone;
+        
+                        string[] resources = new string[] { path }; // Just checking on one resource.
+        
+                        res = RmRegisterResources(handle, (uint)resources.Length, resources, 0, null, 0, null);
+        
+                        if (res != 0) throw new Exception("Could not register resource.");                                    
+        
+                        //Note: there's a race condition here -- the first call to RmGetList() returns
+                        //      the total number of process. However, when we call RmGetList() again to get
+                        //      the actual processes this number may have increased.
+                        res = RmGetList(handle, out pnProcInfoNeeded, ref pnProcInfo, null, ref lpdwRebootReasons);
+        
+                        if (res == ERROR_MORE_DATA)
+                        {
+                            // Create an array to store the process results
+                            RM_PROCESS_INFO[] processInfo = new RM_PROCESS_INFO[pnProcInfoNeeded];
+                            pnProcInfo = pnProcInfoNeeded;
+        
+                            // Get the list
+                            res = RmGetList(handle, out pnProcInfoNeeded, ref pnProcInfo, processInfo, ref lpdwRebootReasons);
+                            if (res == 0)
+                            {
+                                processes = new List<Process>((int)pnProcInfo);
+        
+                                // Enumerate all of the results and add them to the 
+                                // list to be returned
+                                for (int i = 0; i < pnProcInfo; i++)
+                                {
+                                    try
+                                    {
+                                        processes.Add(Process.GetProcessById(processInfo[i].Process.dwProcessId));
+                                    }
+                                    // catch the error -- in case the process is no longer running
+                                    catch (ArgumentException) { }
+                                }
+                            }
+                            else throw new Exception("Could not list processes locking resource.");                    
+                        }
+                        else if (res != 0) throw new Exception("Could not list processes locking resource. Failed to get size of result.");                    
+                    }
+                    finally
+                    {
+                        RmEndSession(handle);
+                    }
+        
+                    return processes;
+                }
+            }
+        }
+"@
+
+        $CheckMyCoreUtilsFileLockUtilLoaded = $CurrentlyLoadedAssemblies | Where-Object {$_.ExportedTypes -like "MyCore.Utils.FileLockUtil*"}
+        if ($CheckMyCoreUtilsFileLockUtilLoaded -eq $null) {
+            Add-Type -ReferencedAssemblies $ReferencedAssemblies -TypeDefinition $TypeDefinition
+        }
+        else {
+            Write-Verbose "The Namespace MyCore.Utils Class FileLockUtil is already loaded and available!"
+        }
+
+        $Result = [MyCore.Utils.FileLockUtil]::WhoIsLocking($FilePath)
+    }
+    if ($PSVersionTable.Platform -ne $null -and $PSVersionTable.Platform -ne "Win32NT") {
+        $lsofOutput = lsof $FilePath
+
+        function Parse-lsofStrings ($lsofOutput, $Index) {
+            $($lsofOutput[$Index] -split " " | foreach {
+                if (![String]::IsNullOrWhiteSpace($_)) {
+                    $_
+                }
+            }).Trim()
+        }
+
+        $lsofOutputHeaders = Parse-lsofStrings -lsofOutput $lsofOutput -Index 0
+        $lsofOutputValues = Parse-lsofStrings -lsofOutput $lsofOutput -Index 1
+
+        $Result = [pscustomobject]@{}
+        for ($i=0; $i -lt $lsofOutputHeaders.Count; $i++) {
+            $Result | Add-Member -MemberType NoteProperty -Name $lsofOutputHeaders[$i] -Value $lsofOutputValues[$i]
         }
     }
-    return $result
+
+    $Result
+    
+    ##### END Main Body #####
+
 }
