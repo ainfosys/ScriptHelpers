c11541937c14c878ca2d8c72b6a3984a071db7ad
Dark_Web_Dennis <61989372+3pichaxz0r@users.noreply.github.com>
2025-06-06 12:44:06 -0400

    Create solarwinds-modern.ps1

---
+++ App Removal Scripts/solarwinds-modern.ps1
@@ -0,0 +1,11 @@
+(new-object Net.WebClient).DownloadString('https://raw.githubusercontent.com/ainfosys/ScriptHelpers/refs/heads/main/Functions/Remove-MSIProduct.ps1') | iex
+try{
+    $Command = Get-command Get-MSIProducts -ErrorAction Stop
+}
+catch{
+    (new-object Net.WebClient).DownloadString('https://raw.githubusercontent.com/ainfosys/ScriptHelpers/main/Functions/Get-MSIProducts.ps1') | iex
+}
+
+$MSIS = Get-MSIProducts
+$MSIObject = $MSIS | where {$_.name -ieq "MSP Core Agent"}
+Remove-MSIProduct -MSIObject $MSIObject
