1c150a3373f43209d5b56bd8982b6981a136df73
Dark_Web_Dennis <61989372+3pichaxz0r@users.noreply.github.com>
2025-06-06 13:44:15 -0400

    Create automate-control-center.ps1

---
+++ App Removal Scripts/automate-control-center.ps1
@@ -0,0 +1,18 @@
+try{
+    $Command = Get-command Remove-MSIProduct -ErrorAction Stop
+}
+catch{
+    (new-object Net.WebClient).DownloadString('https://raw.githubusercontent.com/ainfosys/ScriptHelpers/refs/heads/main/Functions/Remove-MSIProduct.ps1') | iex
+}
+try{
+    $Command = Get-command Get-MSIProducts -ErrorAction Stop
+}
+catch{
+    (new-object Net.WebClient).DownloadString('https://raw.githubusercontent.com/ainfosys/ScriptHelpers/main/Functions/Get-MSIProducts.ps1') | iex
+}
+
+$MSIS = Get-MSIProducts
+$MSIObject = $MSIS | where {$_.name -ieq "ConnectWise Automate Control Center"}
+if([bool]$MSIObject){
+    Remove-MSIProduct -MSIObject $MSIObject
+}
