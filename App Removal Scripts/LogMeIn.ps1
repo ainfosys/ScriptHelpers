56bbef07e03943fe2e70812ba96dae11c1a70f8f
Dark_Web_Dennis <61989372+3pichaxz0r@users.noreply.github.com>
2025-06-06 15:24:44 -0400

    Create LogMeIn.ps1

---
+++ App Removal Scripts/LogMeIn.ps1
@@ -0,0 +1,20 @@
+try{
+    $Command = Get-command Remove-MSIProduct -ErrorAction Stop
+}
+catch{
+    (new-object Net.WebClient).DownloadString('https://raw.githubusercontent.com/ainfosys/ScriptHelpers/refs/heads/main/Functions/Remove-MSIProduct.ps1') | iex
+}
+try{
+    $Command = Get-command Get-MSIProducts -ErrorAction Stop
+}
+catch{
+    (new-object Net.WebClient).DownloadString('https://raw.githubusercontent.com/ainfosys/ScriptHelpers/main/Functions/Get-MSIProducts.ps1') | iex
+}
+
+$MSIS = Get-MSIProducts
+$MSIObject = $MSIS | where {$_.name -ilike "LogMeIn*"}
+if([bool]$MSIObject){
+    foreach($Object in $MSIObject){
+        Remove-MSIProduct -MSIObject $Object
+    }
+}
