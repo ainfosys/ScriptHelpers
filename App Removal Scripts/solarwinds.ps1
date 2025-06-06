478798ffee929019a17b173a9e416e828ce10776
Dark_Web_Dennis <61989372+3pichaxz0r@users.noreply.github.com>
2025-06-06 12:14:31 -0400

    Create solarwinds.ps1
    Solarwinds agent removal script

---
+++ App Removal Scripts/solarwinds.ps1
@@ -0,0 +1,17 @@
+$agentPaths = $(Join-path ${Env:ProgramFiles(x86)} "Advanced Monitoring Agent GP"), $(Join-path ${Env:ProgramFiles(x86)} "Advanced Monitoring Agent")
+
+foreach ($path in $agentPaths) {
+    if (Test-Path -Path "$path\winagent.exe") {
+        if(test-path "$path\settings.ini"){
+            Rename-Item -Path "$path\settings.ini" -NewName "delsettings.ini"
+        }
+        $Process = start-process "$path\winagent.exe" -argumentlist "/removequiet" -wait -passthru
+        try{
+          $Command = get-command Translate-ExitCode -erroraction stop
+          Translate-ExitCode -Process $Process -AutoOutput
+        }
+        catch{
+          write-output "Uninstall process exited with code: $($Process.exitcode)"
+        }
+    }
+}
