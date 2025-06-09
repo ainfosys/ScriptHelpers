43287f4b1ba36fae7cb6cb39dc3c17184c654e98
Dark_Web_Dennis <61989372+3pichaxz0r@users.noreply.github.com>
2025-06-09 13:13:41 -0400

    Create DattoRMM.ps1

---
+++ App Removal Scripts/DattoRMM.ps1
@@ -0,0 +1,14 @@
+try{
+    $Command = Get-command Translate-ExitCode -ErrorAction Stop
+}
+catch{
+    (new-object Net.WebClient).DownloadString('https://raw.githubusercontent.com/ainfosys/ScriptHelpers/main/Functions/Translate-ExitCode.ps1') | iex
+}
+$Uninstallers = Resolve-Path "$($env:ProgramFiles)*\CentraStage\uninst.exe"
+$Arguments = @"
+/VERYSILENT /NORESTART
+"@
+foreach($Uninstaller in $Uninstallers) {
+    $Process = Start-Process "$Uninstaller" -ArgumentList $Arguments -Passthru -wait
+    Translate-ExitCode -process $Process -AutoOutput
+}
