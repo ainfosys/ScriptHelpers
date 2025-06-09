c6cbba633c06275d8d7d21dfa58a26148c9b06c5
Dark_Web_Dennis <61989372+3pichaxz0r@users.noreply.github.com>
2025-06-09 12:09:13 -0400

    Create GenericMSI.ps1

---
+++ App Removal Scripts/GenericMSI.ps1
@@ -0,0 +1,26 @@
+param(
+  $MSIName
+)
+try{
+    $Command = Get-command Remove-MSIProduct -ErrorAction Stop
+}
+catch{
+    (new-object Net.WebClient).DownloadString('https://raw.githubusercontent.com/ainfosys/ScriptHelpers/refs/heads/main/Functions/Remove-MSIProduct.ps1') | iex
+}
+try{
+    $Command = Get-command Get-MSIProducts -ErrorAction Stop
+}
+catch{
+    (new-object Net.WebClient).DownloadString('https://raw.githubusercontent.com/ainfosys/ScriptHelpers/main/Functions/Get-MSIProducts.ps1') | iex
+}
+
+$MSIS = Get-MSIProducts
+$MSIObject = $MSIS | where {$_.name -ilike $MSIName}
+return $MSIObject
+<#
+if([bool]$MSIObject){
+    foreach($Object in $MSIObject){
+        Remove-MSIProduct -MSIObject $Object
+    }
+}
+#>
