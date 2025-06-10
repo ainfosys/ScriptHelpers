f5b300f15b28e9791bd3404f1869cbcdc06cc447
Dark_Web_Dennis <61989372+3pichaxz0r@users.noreply.github.com>
2025-06-10 09:44:09 -0400

    Update New-FolderRecursive.ps1

--- Functions/New-FolderRecursive.ps1
+++ Functions/New-FolderRecursive.ps1
@@ -3,12 +3,32 @@ function New-FolderRecursive {
         $path
     )
 
+    # unable to use get-item if path doesnt exist
+    if ($Path -match "^([A-Z]:|\\\\)(.*)$" ) {
+        $PathType =  "Filesystem"
+    } elseif ($Path -match "^(HKCU:|HKLM:|HKCR:|HKU:|HKCC:|Registry::)(.*)$" ) {
+        $PathType =  "Registry"
+    } else {
+        $PathType =  "Unknown"
+    }
+
     if(!(test-path $path)){
         $parentPath = Split-Path $path -Parent
         $leaf = Split-Path $path -Leaf
         if(!(test-path $parentPath)){
             New-FolderRecursive -path $parentPath
         }
-        New-Item -path $parentPath -Name $leaf -ItemType Directory -Force | Out-Null
+        switch($PathType){
+            "Registry"{
+                New-Item -path $parentPath -Name $leaf -Force | Out-Null
+            }
+            "FileSystem"{
+                New-Item -path $parentPath -Name $leaf -ItemType Directory -Force | Out-Null
+            }
+            default{
+                Write-Warning "Unexpected pathtype: $pathtype"
+                New-Item -path $parentPath -Name $leaf -ItemType Directory -Force | Out-Null
+            }
+        }
     }
 }
