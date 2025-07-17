124c6829fa818f1755152b2c9259e021d3f1c0dd
Ryan <61989372+3pichaxz0r@users.noreply.github.com>
2025-07-17 10:57:05 -0400

    Create BitdefenderEST.ps1

---
+++ App Removal Scripts/BitdefenderEST.ps1
@@ -0,0 +1,6 @@
+#Bitdefender Endpoint Security Tools
+$URL = "https://download.bitdefender.com/SMB/Hydra/release/bst_win/uninstallTool/BEST_uninstallTool.exe"
+$UninstallTool = "$ENV:SystemDrive\Windows\temp\$(split-path $URL -Leaf)"
+Invoke-WebRequest -UseBasicParsing -Uri $URL -OutFile $UninstallTool -ErrorAction Stop
+$process = start-process $UninstallTool -ArgumentList "/bruteForce /destructive /noWait" -Wait -PassThru
+Write-Output "Bitdefender EST removal tool exited with code: $($Process.ExitCode)"
